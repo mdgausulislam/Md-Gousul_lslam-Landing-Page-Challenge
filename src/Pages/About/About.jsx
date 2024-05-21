ccf8c1ebffb49847bdb0ea50357ddf8472a698bb
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2024-05-21 13:23:54 +0600

    responsive glass about section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,19 +4,16 @@ import glass from '../../assets/Image.png'
 
 const About = () => {
     return (
-        <div>
-            <div className="card bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h6 className="card-title pb-5">Experience clear vision with your his <br /> OptiChroma Vision Frames</h6>
-                    <p>At OptiChroma, we believe that everyone deserves clear vision <br />and stylish eyewear. That's why we've designed a collection of <br /> frames that prioritize both form and function.</p>
-                    <p className='pt-2'>Our team of experts is dedicated to helping you find the perfect <br /> pair of glasses.</p>
-
-                    <button class="buyButton1">About US</button>
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h6 className="card-title pb-5">Experience clear vision with <br /> OptiChroma Vision Frames</h6>
+                <p>At OptiChroma, we believe that everyone deserves clear vision <br />and stylish eyewear. That's why we've designed a collection of <br /> frames that prioritize both form and function.</p>
+                <p className='pt-2'>Our team of experts is dedicated to helping you find the perfect <br /> pair of glasses.</p>
+                <div>
+                    <button class="buyButton1 md:ml-[85px]">About US</button>
                 </div>
-                <figure>
-                    <img src={glass} alt="glass" />
-                </figure>
             </div>
+            <figure className='md:w-1/2'><img src={glass} alt="Album" /></figure>
         </div>
     );
 };
