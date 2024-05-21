0962c9ddb8425798bc0f12a4f2188352c731c9fc
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2024-05-21 23:28:50 +0600

    header cart dynamically product added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import './App.css'
 import Header from './Components/Header/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
+import { CartProvider } from './Context/CartContext'
 
 function App() {
 
   return (
-    <>
+    <CartProvider>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </CartProvider>
   )
 }
 
